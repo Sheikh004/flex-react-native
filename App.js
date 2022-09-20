0d7a2bf35b670e70bfc4b7e407cb082a82fd819c
Sheikh Areeb <67228908+Fighter15c@users.noreply.github.com>
2022-09-20 09:16:23 +0500

    my second commit

--- App.js
+++ App.js
@@ -1,11 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {/* <Text>Open up App.js to start working on your app! Thats Cool</Text>
+      <Image style={styles.imageStyle} source={require("./assets/icon.png")} />
+      <TextInput
+        style={{
+          backgroundColor: "lightgrey",
+          marginBottom: 20,
+          marginTop: 20,
+        }}
+      ></TextInput>
+      <TouchableOpacity style={{ backgroundColor: "#DDDDDD", padding: 10 }}>
+        <Text style={{ width: 220, backgroundColor: "pink" }}>Button</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" /> */}
+      <View style={{ flex: 0.2, backgroundColor: "red" }}>
+        <Image
+          source={require("./assets/icon.png")}
+          style={{
+            width: 100,
+            height: 100,
+            alignSelf: "center",
+            marginTop: 20,
+          }}
+        ></Image>
+      </View>
+      <View
+        style={{ flex: 0.7, backgroundColor: "blue", alignItems: "center" }}
+      >
+        <TextInput
+          style={{
+            width: 100,
+            height: 30,
+            backgroundColor: "gray",
+            marginBottom: 30,
+            marginTop: 20,
+          }}
+          placeholder="name"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: 100,
+            height: 30,
+            backgroundColor: "gray",
+            marginBottom: 30,
+          }}
+          placeholder="password"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: 100,
+            height: 30,
+            backgroundColor: "gray",
+            marginBottom: 30,
+          }}
+          placeholder="email"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: 100,
+            height: 30,
+            backgroundColor: "gray",
+            marginBottom: 30,
+          }}
+          placeholder="Gender"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: 100,
+            height: 30,
+            backgroundColor: "gray",
+            marginBottom: 30,
+          }}
+          placeholder="City"
+        ></TextInput>
+      </View>
+      <View style={{ flex: 0.1, backgroundColor: "orange" }}>
+        <TouchableOpacity style={{ width: 400, height: 300 }}>
+          <Text>S ign UP</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +99,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+  imageStyle: {
+    width: 400,
+    height: 400,
+    backgroundColor: "green",
   },
 });
